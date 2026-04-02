79622180d893f4fae3a701ce82cf1b779e4693a5
MaximilianoBurlando <buralndomaxi@gmail.com>
2026-04-02 13:34:11 -0300

    Boton IG

--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -5,7 +5,16 @@ export default function AboutUs() {
       <h1 className="text-3xl font-bold mb-4">
         El Rayo Del Laser
       </h1>
-
+      <a
+        href="https://www.instagram.com/elrayodellaser?igsh=MXc5aTN4MjMxa215dg=="
+        target="_blank"
+        rel="noopener noreferrer"
+        className="inline-block mt-2 px-6 py-2 rounded-full text-white font-semibold 
+           bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 
+           hover:scale-105 transition transform"
+      >
+        📸 Seguinos en Instagram
+      </a>
       <p className="text-gray-700 mb-4">
         Bienvenido a El Rayo Del Laser. Nos especializamos en productos
         personalizados realizados con tecnología de corte y grabado láser.
